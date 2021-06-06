7bd275fb8e118c2569fd4a2d061aae0f9b50ec00
fiftysev <nesgorai@bk.ru>
2021-06-06 16:59:46 +1000

    add props for PlaceCard

--- src/components/PlaceCard.vue
+++ src/components/PlaceCard.vue
@@ -1,12 +1,42 @@
 <template>
   <div class="container shadow-md">
-    <h3>Test</h3>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PlaceCard'
+  name: 'PlaceCard',
+  props: {
+    title: {
+      type: String,
+      default: 'ДВФУ'
+    },
+    image: {
+      type: String,
+      default: 'unsplash.com'
+    },
+    address: {
+      type: String,
+      default: 'Адрес'
+    },
+    description: {
+      type: String,
+      default: 'Описание'
+    },
+    bus_station: {
+      type: String,
+      default: 'Некрасовская, 50'
+    },
+    rating: {
+      type: Number,
+      default: 5.0
+    },
+    tags: {
+      type: Array,
+      default: null
+    }
+  }
 }
 </script>
 
