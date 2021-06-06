4917468b4bd54e6b77dbad53ef871eb497cc4f51
fiftysev <nesgorai@bk.ru>
2021-06-06 15:56:39 +1000

    add PlaceCard component and router link to view it in ChoiceComponent

--- src/components/ChoiceComponent.vue
+++ src/components/ChoiceComponent.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="card w-1/4 bg-green-400 py-5 rounded-md">
+  <div class="card w-1/4 bg-green-400 py-5 rounded-md flex flex-col items-center">
     <select
       name="placepicker"
       id="place"
       v-model="selected"
-      class="w-11/12 bg-white opacity-30"
+      class="w-11/12 bg-white bg-opacity-30"
     >
       <option
         v-bind:value="option.value"
@@ -14,11 +14,25 @@
         {{option.text}}
       </option>
     </select>
+    <router-link
+      tag="a"
+      to="/place/:category"
+      class="button"
+    >
+      <button
+        class="rounded-md bg-blue-400 text-white font-bold mt-3 py-2 px-3"
+        @click.prevent
+      >Начать поиск</button>
+    </router-link>
+    <span
+      class="font-bold text-white underline mt-3 cursor-pointer"
+    >Отменить выбор</span>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'ChoiceComponent',
   data () {
     return {
       selected: null,
@@ -27,8 +41,9 @@ export default {
       ]
     }
   },
-  async mounted () {
-    this.options = (await fetch('api/categories')).json()
+  async created () {
+    const response = await fetch('/api/categories')
+    this.options = await response.json()
   }
 }
 </script>
---
+++ src/components/PlaceCard.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "PlaceCard"
+}
+</script>
+
+<style scoped>
+
+</style>
