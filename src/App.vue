d96428b8d2ef086bff492dc40b03a1366119c508
STR WATCHER <reddreadthomaas@yandex.ru>
2021-06-08 00:10:20 +1000

    App.vue| returning to homepage by clicking on logo was partially added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="app container--fluid main grid min-h-screen place-items-center">
       <div class="navbar">
-        <h2 class="font-bold text-white text-2xl">С Л Е Д У Й З А М Н О Й</h2>
+        <h2 class="font-bold text-white text-2xl">
+          <!--Вот тут роутер не подгружает ChoiceComponent-->
+          <router-link tag="a" :to="{name: '/'}">
+            С Л Е Д У Й З А М Н О Й
+          </router-link>
+        </h2>
       </div>
       <div class="container grid place-items-center min-h-screen">
         <router-view></router-view>
