44b177cc3fd011db7c0b2671f2bf082dc51bc792
Mike Litvin <mrlitvinm@gmail.com>
2021-06-08 00:03:59 +1000

    added Place model

--- followme/randomizer/models.py
+++ followme/randomizer/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class Place(models.Model):
+    place_name = models.CharField(max_length=120)
+    place_photo = models.URLField(max_length=300)
+    place_address = models.CharField(max_length=300, blank=True)
+    place_station = models.CharField(max_length=120, blank=True)
+    place_description = models.TextField()
+    place_rating = models.FloatField()
+    place_url = models.URLField(max_length=300, blank=True)
+    place_tags = TaggableManager()
+
+    def __str__(self):
+        return f'[{self.place_name}]'
