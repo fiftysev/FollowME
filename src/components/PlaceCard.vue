7a7f2cd50f80a3548c5b536415b9e1a38d7f2c3e
STR WATCHER <reddreadthomaas@yandex.ru>
2021-06-07 20:47:03 +1000

    PlaceCard.vue| place card was stylized

--- src/components/PlaceCard.vue
+++ src/components/PlaceCard.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="container shadow-md flex justify-between">
-    <img :src="image" alt="">
-    <div class="container flex flex-col justify-between">
-      <h3>{{title}}</h3>
+  <div class="container shadow-md flex bg-white rounded justify-between">
+    <div class="w-1/3 p-8 flex flex-col text-center">
+      <div class="container pb-full overflow-hidden relative mb-4">
+        <img :src="image" alt="" class="rounded-2xl absolute h-full w-full object-cover">
+      </div>
+
+      <span class="py-4">{{address}}</span>
+      <hr>
+      <span class="py-4">{{bus_station}}</span>
+      <hr>
+    </div>
+
+    <div class="w-2/3 flex flex-col justify-around p-8 text-left">
+      <div class="container flex justify-between">
+        <h3 class="text-xl">{{title}}</h3>
+        <h4>{{rating}}</h4>
+      </div>
+      <hr>
       <p>{{description}}</p>
-      <span>{{address}}</span>
-      <span>{{bus_station}}</span>
-      <h4>{{rating}}</h4>
-      <span v-for="tag in tags" v-bind:key="tag" >{{tag}}</span>
+      <hr>
+      <div class="container flex">
+        <span v-for="tag in tags" v-bind:key="tag" class="p-1">{{tag}}</span>
+      </div>
     </div>
   </div>
 </template>
