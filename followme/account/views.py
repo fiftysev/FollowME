95472a39cd3791565a0f608217b7d3917499e521
Mike Litvin <mrlitvinm@gmail.com>
2021-06-08 17:21:58 +1000

    created "account" app / created registration view

---
+++ followme/account/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+
+def registration(request):
+    data = {}
+    if request.method() == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            data['form'] = form
+            data['res'] = 'success'
+            return render(request, 'pages/registration.html', data)
+        else:
+            form = UserCreationForm()
+            data['form'] = form
+            return render(request, 'pages/registration.html', data)
