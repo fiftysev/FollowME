2f1caae79c93f3b034bcbee419618e5166e03b4f
STR WATCHER <reddreadthomaas@yandex.ru>
2021-06-11 02:27:30 +1000

    tailwind.config.js| custom breakpoints, width and min height properties added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,35 @@ module.exports = {
     './src/**/*.vue'
   ],
   theme: {
+    screens: {
+      'sm': {
+        'min': '641px'
+      },
+      'md': {
+        'min': '769px'
+      },
+      'lg': {
+        'min': '1025px'
+      },
+      'xl': {
+        'min': '1281px'
+      },
+      '2xl': {
+        'min': '1537px'
+      }
+    },
     extend: {
       padding: {
         full: '100%'
       },
       width: {
-        full: '100%'
+        '128': '32rem'
       },
-      height: {
-        full: '100%'
+      minHeight: {
+        main: '90vh',
+        navbar: '5vh'
       }
-    }
+    },
   },
   variants: {},
   plugins: [
