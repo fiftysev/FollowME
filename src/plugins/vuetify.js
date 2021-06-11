742a99a9c5060a17791038c43d06918aec1b5745
STR WATCHER <reddreadthomaas@yandex.ru>
2021-06-11 17:26:05 +1000

    vuetify.js| setup of custom breakpoints

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib/framework'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  breakpoint: {
+    thresholds: {
+      sm: 641,
+      md: 769,
+      lg: 1025,
+      xl: 1281,
+      '2xl': 1537
+    }
+  }
 })
