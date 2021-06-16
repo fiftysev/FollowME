c4dcc1996a0847f496c4781458eff85d447b025e
Mike Litvin <mrlitvinm@gmail.com>
2021-06-16 19:57:26 +1000

    added crud functions / deleted app.py / updated main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
-import uvicorn
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from .database import engine
+from . import schemas, crud, models
 
-if __name__ == '__main__':
-    uvicorn.run(
-        "app:app",
-        host='localhost',
-        port=8080,
-        reload=True
-    )
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("auth/signup/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_username(db, username=user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    return crud.create_user(db=db, user=user)
